9ceaf294f21ece7446de8cdc72d347f9246d6e1b
rajithsuvarna <rajithsuvarna49@gmail.com>
2023-09-15 09:28:56 +0530

    project completed

--- todo.js
+++ todo.js
@@ -1,9 +1,13 @@
+//creating tasks array
 let tasks = [];
+
+//initializing values using ID
 const taskList = document.getElementById("list");
 var addTaskInput = document.getElementById("todo-input");
 const tasksCounter = document.getElementById("tasks-counter");
 const addbutton = document.getElementById("addtask");
 
+//function to add task to Dom
 function addToDom(task) {
   const li = document.createElement("li");
   li.innerHTML = `<input type="checkbox" id='${task.id}' data-id="${task.id}" ${
@@ -15,6 +19,7 @@ function addToDom(task) {
   taskList.append(li);
 }
 
+//function to render the tasks
 function renderList() {
   taskList.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
@@ -23,6 +28,7 @@ function renderList() {
   tasksCounter.innerHTML = tasks.length;
 }
 
+//function mark the tasks as completed
 function markTaskAsComplete(taskId) {
   const completedTask = tasks.filter(function (task) {
     return task.id === taskId;
@@ -34,6 +40,8 @@ function markTaskAsComplete(taskId) {
     return;
   }
 }
+
+//function to filter all tasks
 function AllTasks() {
   taskList.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
@@ -41,6 +49,8 @@ function AllTasks() {
   }
   tasksCounter.innerHTML = tasks.length;
 }
+
+//function to filter completed tasks
 function CompletedTasks() {
   taskList.innerHTML = "";
   const completedTask = tasks.filter(function (task) {
@@ -50,6 +60,8 @@ function CompletedTasks() {
     addToDom(completedTask[i]);
   }
 }
+
+//function to filter pending tasks
 function NotCompletedTasks() {
   taskList.innerHTML = "";
   const pendingTask = tasks.filter(function (task) {
@@ -60,6 +72,7 @@ function NotCompletedTasks() {
   }
 }
 
+//function to delete completed tasks
 function deleteCompletedTasks() {
   const newTasks = tasks.filter(function (task) {
     return task.done === false;
@@ -68,6 +81,7 @@ function deleteCompletedTasks() {
   renderList();
 }
 
+//function to delete specific task
 function deleteTask(taskId) {
   const newTasks = tasks.filter(function (task) {
     return task.id !== taskId;
@@ -75,11 +89,14 @@ function deleteTask(taskId) {
   tasks = newTasks;
   renderList();
 }
+
+//fuction to delete all tasks
 function DeleteAllTasks() {
   tasks = [];
   renderList();
 }
 
+//function to add tasks
 function addTask(task) {
   if (task) {
     tasks.push(task);
@@ -88,6 +105,7 @@ function addTask(task) {
   }
 }
 
+//fuction to create tasks
 function createTask() {
   const text = addTaskInput.value;
   if (text) {
@@ -101,8 +119,10 @@ function createTask() {
   }
 }
 
+//add button listener
 addbutton.addEventListener("click", createTask);
 
+//adding event listener
 document.addEventListener("click", function (e) {
   const target = e.target;
   if (target.id === "delete") {
